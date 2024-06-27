c70c982f01b96c54ee5d25860734e54f60c4e22f
Lc9real <lukacondric7@gmail.com>
2024-06-27 19:31:03 +0200

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.net.Socket;
+import java.util.List;
+
 public class Main {
+    
+    public static String ipAdress = "localhost";
+    public static int port = 9806;
+    static Socket socket;
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, im the Client!");
+        try
+        {
+           
+
+            System.out.println("Starting Client...");
+            socket = new Socket(ipAdress, port);
+            System.out.println("Connected to Server :)");
+
+            ServerRequest serverRequest = new ServerRequest(socket);
+
+
+
+            
+
+             
+            List<Post> posts = serverRequest.getPosts(10, 1, SortBy.VOTES);
+            for(Post post : posts)
+            {
+                System.out.println(post);
+            }
+
+        
+
+
+            
+
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        finally
+        {
+            socket.close();
+        }
+    }
+
+
+    
+    private String test()
+    {
+        return "";
     }
 }
---
+++ src/ServerRequest.java
@@ -0,0 +1,138 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+
+public class ServerRequest 
+{
+    public ObjectOutputStream outputStream;
+    public ObjectInputStream inputStream;
+    
+    
+    public ServerRequest(Socket socket) throws Exception
+    {
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+        inputStream = new ObjectInputStream(socket.getInputStream());
+    }
+    
+
+    public void addPost(Post post) throws Exception
+    {
+        Object[] arg = new Object[] {post};
+        Message message = new Message(1,"addPost", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+    }
+
+    public Post getPostInfo(int postID) throws Exception
+    {
+        Object[] arg = new Object[] {postID};
+        Message message = new Message(1,"getPostInfo", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (Post)answer.data;
+    }
+
+    public List<Post> getPosts(int count, Integer series, SortBy sortBy) throws Exception
+    {
+        Object[] arg = new Object[] {count, series, sortBy};
+        Message message = new Message(1,"getPosts", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (List<Post>)answer.data;
+    }
+
+    public void addComment(Comment comment) throws Exception
+    {
+        Object[] arg = new Object[] {comment};
+        Message message = new Message(1,"addComment", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+    }
+
+
+    public Comment getCommentInfo(int commentID) throws Exception
+    {
+        Object[] arg = new Object[] {commentID};
+        Message message = new Message(1,"getCommentInfo", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (Comment)answer.data;
+    }
+
+    public List<Comment> getComments(int count, int postId, SortBy sortBy) throws Exception
+    {
+        Object[] arg = new Object[] {count, postId, sortBy};
+        Message message = new Message(1,"getComments", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (List<Comment>)answer.data;
+    }
+
+    public List<Comment> getSubComments(int count, int parentID, SortBy sortBy) throws Exception
+    {
+        Object[] arg = new Object[] {count, parentID, sortBy};
+        Message message = new Message(1,"getSubComments", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (List<Comment>)answer.data;
+    }
+
+    public List<Comment.CommentLevel> getAllSubComments(List<Comment> comments, int indentLevel) throws Exception
+    {
+        Object[] arg = new Object[] {comments, indentLevel};
+        Message message = new Message(1,"getAllSubComments", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (List<Comment.CommentLevel>)answer.data;
+    }
+
+    public Integer getSeriesID(String name) throws Exception
+    {
+        Object[] arg = new Object[] {name};
+        Message message = new Message(1,"getSeriesID", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (Integer)answer.data;
+    }
+
+
+    public Series getSeriesInfo(int id) throws Exception
+    {
+        Object[] arg = new Object[] {id};
+        Message message = new Message(1,"getSeriesInfo", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (Series)answer.data;
+    }
+
+    public Integer getUserIDFromUsername(String username) throws Exception
+    {
+        Object[] arg = new Object[] {username};
+        Message message = new Message(1,"getUserIDFromUsername", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (Integer)answer.data;
+    }
+
+    public User getUserInfo(int userID) throws Exception
+    {
+        Object[] arg = new Object[] {userID};
+        Message message = new Message(1,"getUserInfo", arg);
+        outputStream.writeObject(message);
+        outputStream.flush();
+        Message answer = (Message)inputStream.readObject();
+        return (User)answer.data;
+    }
+
+    
+}
